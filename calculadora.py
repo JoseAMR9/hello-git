6329d67b96244fb7842ce10b7a074aae0da6eafc
Jose Marcelo <jamarcelor07@gmail.com>
2026-04-06 10:09:42 -0500

    añadiendo funcion de multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -5,10 +5,12 @@ print("1.- Suma")
 print("2.- Resta")
 print("3.- Multiplicacion")
 print("4.- Division")
-opcion = int(input("Elija una opcion: "))
+opcion = int(input("Seleccione una opcion: "))
 
 match opcion:
     case 1:
         print(f"La suma es: {x + y}")
     case 2:
         print(f"La resta es: {x + y}")
+    case 3:
+        print(f"La multiplicacion es: {x * y}")
